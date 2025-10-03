01112f5ac9777bf0641c7d06523d8201626d7436
giozu <giovanni.zullo@polimi.it>
2025-10-03 19:45:38 +0200

    riscritto

--- tests/exercises/inco_porous_polycrystal_generator.py
+++ tests/exercises/inco_porous_polycrystal_generator.py
@@ -9,7 +9,7 @@ USE_AMITEX = False   # set True to run Amitex after voxelization
 
 # --- Geometry and discretization
 L_RVE = 10
-n3D = 300                       # Number of voxels per axis
+n3D = 100                       # Number of voxels per axis
 
 seed = 0                        # RNG seed for reproducibility
 
@@ -65,7 +65,7 @@ def Crack_structure_Voxellation(n3D, length, seed, delta, voxel_rule, K, vtkname
 
     """
 
-    # --- Step 1: Intergranular spherical inclusions (large pores)
+    # --- Step 1: Intergranular spherical multiInc_intergranular (large pores)
     intergranular = merope.SphereInclusions_3D()
     intergranular.setLength(length)
     intergranular.fromHisto(
@@ -76,11 +76,11 @@ def Crack_structure_Voxellation(n3D, length, seed, delta, voxel_rule, K, vtkname
         [incl_phase]
     )
 
-    # pores of phase 2 (incl_phase)
-    multiInclusions2 = merope.MultiInclusions_3D()
-    multiInclusions2.setInclusions(intergranular)
+    # pores of incl_phase)
+    multiInc_intergranular = merope.MultiInclusions_3D()
+    multiInc_intergranular.setInclusions(intergranular)
 
-    # --- Step 2: Intragranular spherical inclusions (small pores)
+    # --- Step 2: Intragranular spherical multiInc_intergranular (small pores)
     intragranular = merope.SphereInclusions_3D()
     intragranular.setLength(length)
     intragranular.fromHisto(
@@ -93,10 +93,10 @@ def Crack_structure_Voxellation(n3D, length, seed, delta, voxel_rule, K, vtkname
     )
 
     # pores of phase 1 (temp_phase)
-    multiInclusions1 = merope.MultiInclusions_3D()
-    multiInclusions1.setInclusions(intragranular)
+    multiInc_intragranular = merope.MultiInclusions_3D()
+    multiInc_intragranular.setInclusions(intragranular)
 
-    # --- Step 3: Laguerre tessellation for grains
+    # # --- Step 3: Laguerre tessellation for grains
     grain = merope.SphereInclusions_3D()
     grain.setLength(length)
     grain.fromHisto(
@@ -106,31 +106,34 @@ def Crack_structure_Voxellation(n3D, length, seed, delta, voxel_rule, K, vtkname
         [[R_grain, P_grain]], 
         [temp_phase] # anche i grani in phase 1 (temp)
     )
+
     polyCrystal = merope.LaguerreTess_3D(length, grain.getSpheres())
 
-    multiInclusions = merope.MultiInclusions_3D()
-    multiInclusions.setInclusions(polyCrystal)
+    multiInc_grain = merope.MultiInclusions_3D()
+    multiInc_grain.setInclusions(polyCrystal)
 
     # Add boundary layer of thickness delta to the grains, and phase 1 (temp)
-    multiInclusions.addLayer(multiInclusions.getAllIdentifiers(), delta_phase, delta) # layer in delta phase
-    multiInclusions.changePhase(multiInclusions.getAllIdentifiers(), [temp_phase for _ in multiInclusions.getAllIdentifiers()])
+    multiInc_grain.addLayer(multiInc_grain.getAllIdentifiers(), delta_phase, delta) # layer in delta phase
+    multiInc_grain.changePhase(multiInc_grain.getAllIdentifiers(), [temp_phase for _ in multiInc_grain.getAllIdentifiers()]) # grani tutti nella stessa fase
+    # multiInc_grain.changePhase(multiInc_grain.getAllIdentifiers(),multiInc_grain.getAllIdentifiers()) # grain in fasi diverse (per vederli su paraview)
 
     # --- Step 4: Merge structures
     structure3 = merope.Structure_3D(
-        multiInclusions2, 
-        multiInclusions, 
+        multiInc_intergranular, # inter-granulari, phase 2 (incl)
+        multiInc_grain,  # grain (phase 1) + delta (phase 3)
         {incl_phase: grain_phase,  # old phase --> new phase
          delta_phase: grain_phase} # old phase --> new phase
     )
 
     structure  = merope.Structure_3D(
         structure3, 
-        multiInclusions1, 
+        multiInc_intragranular, 
         {grain_phase: incl_phase}
     )
 
     # --- Step 5: Voxelization
-    gridParams = merope.vox.create_grid_parameters_N_L_3D([n3D, n3D, n3D], [L_RVE, L_RVE, L_RVE])
+    voxellation = [n3D, n3D, n3D]
+    gridParams = merope.vox.create_grid_parameters_N_L_3D(voxellation, [L_RVE, L_RVE, L_RVE])
     grid = merope.vox.GridRepresentation_3D(structure, gridParams, voxel_rule)
 
     # --- Step 6: Print resolution parameters
